3d2ea128c67dd9c85b6fb9863605a304f272c839
Ryley Hindman <cessnageek@gmail.com>
2021-07-02 23:04:48 -0500

    First stab at line track software.  It is untested, but does not give errors.

--- GUI_Latest.py
+++ GUI_Latest.py
@@ -9,6 +9,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene,\
 from PyQt5.QtGui import QPen, QBrush
 from PyQt5.Qt import Qt
  
+import numpy
+
 from mpmath import *
 mp.dps = 20
 print(mp)
@@ -19,6 +21,7 @@ import time
 import serial
 import numpy
 import math
+import copy
 
 from threading import Lock
 from queue import Queue
@@ -26,6 +29,7 @@ from queue import Queue
 data_lock = Lock()
 quit_lock = Lock()
 point_lock = Lock()
+
 newPost = False
 pl1ToCalc = 0
 pl2ToCalc = 0
@@ -58,10 +62,10 @@ class fileWorker(QObject):
 
             if(not fileQueue.empty()):
                 data = fileQueue.get(False)
-                print("Committing1!")
+                #print("Committing1!")
 
                 if(not data == None):
-                    print("Committing2!")
+                #    print("Committing2!")
                     self.commitToFile(data)
             
         self.closeFile()
@@ -230,6 +234,120 @@ def calcVelocity(a,b,x1,y1,z1,x2,y2,z2,delT):
 
     return v1, v2, v3
 
+class WorldPoint():
+    def __init__(self, xWorld, yWorld, zWorld, empty=False):
+        if (not empty):
+            self.x = xWorld
+            self.y = yWorld
+            self.z = zWorld
+            self.next = None
+        elif(empty):
+            self.x = None
+            self.y = None
+            self.z = None
+            self.next = None
+
+    def add(self, point):
+        self.next = copy.deepcopy(point)
+        return self.next
+
+    def iterate(self):
+        return self.next
+
+    def iterateSafe(self):
+        if(not self.next is None):
+            return self.next, True
+        else:
+            return self, False
+
+    def get(self):
+        return self.x, self.y, self.z
+
+    def clearNext(self):
+        self.next = None
+        return self
+
+class SceneCoordinates():
+    def __init__(self,x0, y0, z0, x1, y1, z1, n1, n2, n3):
+        self.basis = numpy.array([[0,0,0],
+                                [0,0,0],
+                                [0,0,0]])
+
+        self.inverseBasis = numpy.array([[0,0,0],
+                                        [0,0,0],
+                                        [0,0,0]])
+
+        self.readyToTransform = False
+
+        self.origin = numpy.array([[0],[0],[0]])
+
+        self.calcBasis(x0, y0, z0, x1, y1, z1, n1, n2, n3)
+
+
+    #This function calculates the basis from (x0, y0, z0)
+    #(x1, y1, z1) and <n1, n2, n3>
+    def calcBasis(self, x0, y0, z0, x1, y1, z1, n1, n2, n3):
+        v3 = numpy.array([n1,n2,n3])/numpy.linalg.norm([n1, n2, n3])
+        v2 = numpy.array([x1-x0,y1-y0,z1-z0])/numpy.linalg.norm([x1- x0, y1-y0, z1-z0])
+
+        v1 = numpy.cross(v2, v3)
+
+        self.origin = numpy.array([[x0],[y0],[z0]])
+
+        self.setBasis(v1, v2, v3)
+
+
+    #This function sets the basis matrix to the 
+    #basis vectors <v1> <v2> <v3>
+    def setBasis(self, v1, v2, v3):
+        print("V1: ")
+        print(v1)
+        print("V2: ")
+        print(v2)
+        print("V3: ")
+        print(v3)
+        self.basis = numpy.float64(numpy.array([[v1[0],v2[0],v3[0]],
+                                [v1[1],v2[1],v3[1]],
+                                [v1[2],v2[2],v3[2]]]))
+
+        #self.basis[0] = numpy.array(v1)
+        #self.basis[1] = numpy.array(v2)
+        #self.basis[2] = numpy.array(v3)
+        print(self.basis)
+        self.calcInverseBasis()
+
+
+    #This function calculates the inverse matrix of the basis vectors
+    #<v1> <v2> <v3>
+    def calcInverseBasis(self):
+        self.readyToTransform = True
+        try:
+            self.inverseBasis = numpy.linalg.inv(self.basis)
+        except numpy.linalg.LinAlgError as e:
+            print(self.basis)
+            print(e)
+            print("Unable to calculate inverse basis")
+            self.readyToTransform = False
+
+
+    #This function takes care of the coordinate transformation from
+    #world coordinates to local coordinates, with the origin at 
+    #(Xb, Yb, Zb) and basis vectors <Xv, Yv, Zv>^(-1) (input inverse matrix)
+    def calcSceneLocalCoordinates(self, x, y, z):
+        localCoord = numpy.array([[0],[0],[0]])
+        success = False
+        if(self.readyToTransform):
+            relativeCoord = numpy.array([[x],[y],[z]]) - self.origin
+            print("localcoord: ")
+            print(localCoord)
+            localCoord = numpy.matmul(self.inverseBasis, numpy.float64(relativeCoord))
+            success = True
+
+        return localCoord, success
+
+    
+
+
 class Worker(QObject):
     finished = pyqtSignal()
     errorOut = pyqtSignal(float)
@@ -237,6 +355,8 @@ class Worker(QObject):
     distOut = pyqtSignal(float)
     normVectOut = pyqtSignal(tuple)
     qualOut = pyqtSignal(int)
+    localPointOut = pyqtSignal(WorldPoint)
+
     def runMainLoop(self):
         a = 6378137
         b = mpf(6356752.3142)
@@ -278,6 +398,8 @@ class Worker(QObject):
         velTimer = time.time()
         first = 1
         quality = 0
+        listBeginning = WorldPoint(0,0,0,True)
+
         global newPost
         global pl1ToCalc
         global pl2ToCalc
@@ -317,6 +439,7 @@ class Worker(QObject):
                         print("First push")
                         print(str(x1) + " " + str(y1) + " " + str(z1) + " " + str(quality))
                         addToQueue(x1,y1,z1,0,0)
+
                         firstPos = True
                         pointButtonPushed = False
                     elif(not secondPos):
@@ -324,6 +447,25 @@ class Worker(QObject):
                         print("Second push")
                         pl1, pl2, pl3 = calcPlaneThroughOrigin(x1,y1,z1,x2,y2,z2)
                         addToQueue(x2,y2,z2,0,0)
+
+                        t = mpf(math.sqrt((a**2 * b**2)/(x2**2 * b**2 + y2**2 * b**2 + z2**2 * a*2)))
+                        xEllipse = x2 * t
+                        yEllipse = y2 * t
+                        zEllipse = z2 * t
+                        n1 = mpf(2 * xEllipse / (a**2))
+                        n2 = mpf(2 * yEllipse / (a**2))
+                        n3 = mpf(2 * zEllipse / (b**2))
+                        
+                        localScene = SceneCoordinates(x1, y1, z1, x2, y2, z2, n1, n2, n3)
+
+                        #Add 0,0,0 and the local coordinates for x2, y2, z2 to our linked list
+                        pointsLinkedList = listBeginning.add(WorldPoint(0,0,0,True))
+                        points, success = localScene.calcSceneLocalCoordinates(x2,y2,z2)
+                        if(not success):
+                            raise Exception('Local world coordinate calculation unsuccessful')
+                        pointsLinkedList = pointsLinkedList.add(WorldPoint(points[0][0], points[1][0], points[2][0]))
+                        self.localPointOut.emit(pointsLinkedList)
+
                         secondPos = True
                         pointButtonPushed = False
                     elif(not thirdPos):
@@ -335,7 +477,6 @@ class Worker(QObject):
                         thirdPos = True
                         pointButtonPushed = False
                 else:
-                    print("Updating")
                     x, y, z, quality = updatePosition(gps)
 
                 point_lock.release()
@@ -346,6 +487,15 @@ class Worker(QObject):
                 effort = 0 #- for turning left + for turning right
                 kp = 0 #unused for now
                 x, y, z, quality = updatePosition(gps)
+                points, success = localScene.calcSceneLocalCoordinates(x,y,z)
+                if(not success):
+                    raise Exception('Local world coordinate calculation unsuccessful')
+                print("Points to add to linked list")
+                print(str(points[0]) + " " + str(points[1]) + " " + str(points[2]))
+                pointsLinkedList = pointsLinkedList.add(WorldPoint(points[0][0], points[1][0], points[2][0]))
+                self.localPointOut.emit(pointsLinkedList)
+                print("Points linked list")
+                print(pointsLinkedList.get())
                 addToQueue(x,y,z,0,0)
                 data_lock.acquire()
                 if(newPost == True):
@@ -390,9 +540,7 @@ class Worker(QObject):
                 directionSign = initDirV1 * v1 + initDirV2 * v2 + initDirV3 * v3
                 error = mpf(desiredValue) - actualValue
                 distFromPrev = math.sqrt((x-prevXPost)**2 + (y-prevYPost)**2 + (z-prevZPost)**2)
-                #print("Error: " + str(actualValue))
-                #print("x: " + str(x) + "y: " + str(y) + "z: " + str(z))
-                print("pl1: " + str(pl1) + "pl2: " + str(pl2) + "pl3: " + str(pl3))
+
                 steer = 0
                 if (directionSign < 0 and error != 0):
                     # If we are going in the original direction, and error is positive
@@ -431,9 +579,16 @@ class Window(QMainWindow):
         self.left = 500
         self.width = 750
         self.height = 400
+
+        self.sceneModifier = 5
+        self.sceneWidth = int(self.width/2)-self.sceneModifier
+        self.sceneHeight = int(self.height)-self.sceneModifier
+
+
         self.lineWidth = 20
         self.thinLineWidth = 5
         self.veryThinLineWidth = 1
+        self.trackLineWidth = 2
         self.oneFootPixels = int(self.width/8)
         self.error = 0.0
         self.pl1 = mpf(0)
@@ -442,6 +597,25 @@ class Window(QMainWindow):
         self.firstPoint = False
         self.secondPoint = False
         self.thirdPoint = False
+
+        self.worldCoordsInit = False
+        self.xMaxWorld = 0
+        self.xMinWorld = 0
+        self.yMaxWorld = 0
+        self.yMinWorld = 0
+        self.xMax = self.sceneWidth - 10
+        self.xMin = 10
+        self.yMax = self.sceneHeight - 10 + self.sceneModifier
+        self.yMin = self.sceneModifier + 10
+        #List to hold the track lines
+        self.lines = []
+
+        #Point to hold the points
+        self.pointStart = WorldPoint(0,0,0,True)
+        self.pointsIndex = self.pointStart
+        self.redraw = False
+        self.lastX = 0
+        self.lastY = 0
  
         self.InitWindow()
 
@@ -603,10 +777,15 @@ class Window(QMainWindow):
         self.redPen = QPen(Qt.red)
         self.bluePen = QPen(Qt.blue)
         self.blueBrush = QBrush(Qt.blue)
+        self.purplePen = QPen(Qt.magenta)
+        self.purplePen.setWidth(self.trackLineWidth)
+        self.purplePen.setCapStyle(Qt.FlatCap)
 
         graphicView = QGraphicsView(self.scene, self)
+        #If any of these height modifiers are changed, we must also change the modifiers
+        #in drawLine()
         graphicView.setGeometry(int(self.width/2),0,int(self.width/2),int(self.height)+5)
-        graphicView.setSceneRect(0,0,int(self.width/2)-5,int(self.height)-5)
+        graphicView.setSceneRect(0,0,self.sceneWidth,self.sceneHeight)
         self.initShapes()
 
     def initShapes(self):
@@ -629,6 +808,16 @@ class Window(QMainWindow):
         self.tractor = self.scene.addRect(0,0,self.tractorWidth,self.tractorHeight,self.bluePen,self.blueBrush)
         self.tractor.setPos(int(self.width/4)-int(self.tractorWidth/2),\
          int(self.height/2)-int(self.tractorHeight/2))
+
+    def drawLine(self, x1, y1, x2, y2):
+        #Note, this works in coordinates with (0,0) as the bottom left
+        #corner, not the top left corner.
+        if(x1 < 0 or x2 < 0 or y1 < 0 or y2 < 0):
+            print("All inputs must be positive")
+        else:
+            lineLength = sqrt((x2-x1)**2 + (y2-y1)**2)
+            self.lines.append(self.scene.addLine(0,0,int(x2-x1),int(y1-y2),self.purplePen))
+            self.lines[len(self.lines)-1].setPos(x1, (self.height-y1))
 
     def setTractorPos(self, error):
         pixels = int(error * self.width * 0.125)
@@ -666,6 +855,92 @@ class Window(QMainWindow):
         else:
             self.qualityReadout.setText("No Fix")
 
+    def setPoints(self, pointToDraw):
+        if(not self.worldCoordsInit):
+            self.xMinWorld,self.yMinWorld,tmp = pointToDraw.get()
+            #Default distance across = 20m
+            self.xMaxWorld = self.xMinWorld + 20
+            self.yMaxWorld = self.yMinWorld + 20
+
+            #Add the point after clearing the next.  Becasue we do a deep copy,
+            #we don't want to recursively add all of the next points
+            self.pointsIndex = self.pointStart.add(pointToDraw.clearNext())
+        else:
+            xCoord, yCoord,tmp = pointToDraw.get()
+            if(xCoord > self.xMaxWorld):
+                self.xMaxWorld = xCoord
+                self.redraw = True
+            elif(xCoord < self.xMinWorld):
+                self.xMinWorld = xCoord
+                self.redraw = True
+
+            if(yCoord > self.yMaxWorld):
+                self.yMaxWorld = yCoord
+                self.redraw = True
+            elif(yCoord < self.yMinWorld):
+                self.yMinWorld = yCoord
+                self.redraw = True
+
+            #Add the point after clearing the next.  Becasue we do a deep copy,
+            #we don't want to recursively add all of the next points            
+            self.pointsIndex = self.pointsIndex.add(pointToDraw.clearNext())
+
+        self.drawPoints()
+
+    #This function converts the pointToConvert values to the local coordinates
+    #defined by the maximum and minimum world and scene values
+    def convertToLocalCoords(self, pointToConvert):
+        xWorld, yWorld, tmp = pointToConvert.get()
+
+        #Essentially, the equation gets the fraction of the world max/min, and
+        #multiplies that by the length of the scene, plus the scene offset
+        xScene = (xWorld - self.xMinWorld) * (self.xMax - self.xMin) / \
+        (self.xMaxWorld - self.xMinWorld) + self.xMin
+
+        yScene = (yWorld - self.yMinWorld) * (self.yMax - self.yMin) / \
+        (self.yMaxWorld - self.yMinWorld) + self.yMin
+
+        return xScene, yScene
+
+    def drawPoints(self):
+        if(self.redraw):
+            print("Redraw")
+            self.pointsIndex, hasNext = self.pointStart.iterateSafe()
+            firstRedrawPoint = True
+            while(hasNext):
+                if(firstRedrawPoint):
+                    self.lastX, self.lastY = self.convertToLocalCoords(self.pointsIndex)
+                    firstRedrawPoint = False
+                else:
+                    currX, currY = self.convertToLocalCoords(self.pointsIndex)
+                    if(not(currX == self.lastX or currY == self.lastY)):
+                        self.drawLine(self.lastX, self.lastY, currX, currY)
+                    self.lastX = currX
+                    self.lastY = currY
+                self.pointsIndex, hasNext = self.pointsIndex.iterateSafe()
+            self.redraw = False
+        else:
+            print("Not redraw")
+            currX, currY = self.convertToLocalCoords(self.pointsIndex)
+            if(not self.worldCoordsInit):
+                self.lastX = currX
+                self.lastY = currY
+                self.worldCoordsInit = True
+            else:
+                print("Should draw line")
+                if(not(currX == self.lastX or currY == self.lastY)):
+                    print("drawing line")
+                    self.drawLine(self.lastX, self.lastY, currX, currY)
+
+                self.lastX = currX
+                self.lastY = currY
+
+
+
+
+
+
+
     def runMainTask(self):
         self.thread = QThread()
         self.worker = Worker()
@@ -680,6 +955,7 @@ class Window(QMainWindow):
         self.worker.distOut.connect(self.setDist)
         self.worker.normVectOut.connect(self.setNormVect)
         self.worker.qualOut.connect(self.setQualityReadout)
+        self.worker.localPointOut.connect(self.setPoints)
 
         self.thread.start()
 
@@ -703,7 +979,7 @@ class Window(QMainWindow):
 App = QApplication(sys.argv)
 window = Window()
 window.runMainTask()
-window.runSaveTask()
+#window.runSaveTask()
 
 while(keepRunning):
     window.setErrorString(float(window.error * 3.28))
